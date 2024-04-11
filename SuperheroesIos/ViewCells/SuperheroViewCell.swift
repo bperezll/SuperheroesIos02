528890d1eb6dc16f0e985d2f26f55968766ccb59
bperezll <borja.perez.llano@gmail.com>
2024-04-11 11:24:23 +0200

    Development of CollectionView

--- SuperheroesIos/ViewCells/SuperheroViewCell.swift
+++ SuperheroesIos/ViewCells/SuperheroViewCell.swift
@@ -10,32 +10,3 @@ import UIKit
 class SuperheroViewCell: UICollectionViewCell {
     
 }
- /*
-  
-  class HoroscopeViewCell: UITableViewCell {
-      
-      // MARK: Outlets
-      
-      @IBOutlet var titleLabel: UILabel!
-      
-      @IBOutlet var subtitleLabel: UILabel!
-      
-      @IBOutlet var signImageView: UIImageView!
-      
-      @IBOutlet weak var favoriteButton: UIButton!
-      
-      // MARK: Data
-      
-      func render(horoscope: Horoscope) {
-          titleLabel.text = horoscope.name
-          subtitleLabel.text = horoscope.dates
-          signImageView.image = horoscope.image
-          
-          let favoriteHoroscope = UserDefaults.standard.string(forKey: "FAVORITE_HOROSCOPE") ?? ""
-          let isFavorite = horoscope.id == favoriteHoroscope
-          
-          favoriteButton.isHidden = !isFavorite
-      }
-  }
-  
-  */
