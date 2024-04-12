9328c44fdf90daaf09d741dc3d04537fa5417ec0
bperezll <borja.perez.llano@gmail.com>
2024-04-12 10:59:12 +0200

    CollectionView example

--- SuperheroesIos/ViewController.swift
+++ SuperheroesIos/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     var superheroes = SuperheroeProvider.getSuperheroes()
     
+    /*var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +24,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         SuperheroCollectionView.dataSource = self
         
     }
+    
+    /*func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }*/
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return superheroes.count
@@ -34,13 +40,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         print(item.name)
         
         // Constant variable creation connected to identifier
-        let cell: SuperheroViewCell = SuperheroCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SuperheroViewCell
+        let cell: SuperheroViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SuperheroViewCell
         
         // Accessing the superhero fields
         cell.superheroeNameLabel.text = item.name
         cell.superheroeImageView.image = UIImage(named: item.image.url)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
     }
 }
 
